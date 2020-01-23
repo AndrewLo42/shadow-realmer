139375e5f86974576feedadf92bdcd38b0b4a8bc
spenceruns <spenceruns@gmail.com>
2020-01-23 20:41:13 +0000

    Created reusable classless icon component

---
+++ client/components/shadow-realmer-icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ShadowRealmerIcon(props) {
+  return (
+    <div className="icon-container">
+      <img src="/images/shadowrealmer.png" alt="icon" className="icon-image"/>
+    </div>
+  );
+}
+
+export default ShadowRealmerIcon;
