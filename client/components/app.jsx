399939f498e86a1c8f239ff03055e084bc5eebd4
Elliot Park <elliot.jun.park@gmail.com>
2020-01-24 21:29:09 +0000

    First pass at Sidebar component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import HangoutsList from './hangouts-list';
+import SearchBar from './search-bar';
+import Sidebar from './sidebar';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,12 +13,22 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isSidebarHidden: true
     };
+    this.toggleSidebar = this.toggleSidebar.bind(this);
+  }
+
+  toggleSidebar() {
+    this.setState(prevState => ({
+      isSidebarHidden: !prevState.isSidebarHidden
+    }));
   }
 
   render() {
     return (
       <Router>
+        <SearchBar toggleSidebar={this.toggleSidebar} />
+        <Sidebar toggleSidebar={this.toggleSidebar} isSidebarHidden={this.state.isSidebarHidden} />
         <Switch>
           <Route exact path="/hangouts" component={HangoutsList} />
           <Route path="/hangouts/:id" component={HangoutDetails} />
