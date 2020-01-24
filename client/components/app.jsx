10be84046ec8bb5c1bf08eff1167f724c8602917
Elliot Park <elliot.jun.park@gmail.com>
2020-01-24 02:32:51 +0000

    Basic routing between HangoutDetails and HangoutList

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import HangoutsList from './hangouts-list';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import HangoutDetails from './hangout-details';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,6 +15,13 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <HangoutsList />;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/hangouts" component={HangoutsList} />
+          <Route path="/hangouts/:id" component={HangoutDetails} />
+        </Switch>
+      </Router>
+    );
   }
 }
