17782017813e2826d25f07780e488a52a9e0cdb2
Elliot Park <elliot.jun.park@gmail.com>
2020-01-29 00:32:13 +0000

    Added back button and minor error handling for log in page

--- client/components/log-in-page.jsx
+++ client/components/log-in-page.jsx
@@ -6,7 +6,8 @@ export default class LogInPage extends React.Component {
     super(props);
     this.state = {
       userName: '',
-      password: ''
+      password: '',
+      hasUserUnsuccessfullyLoggedIn: false
     };
     this.handleUserNameChange = this.handleUserNameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -35,7 +36,17 @@ export default class LogInPage extends React.Component {
       body: requestBody
     };
     fetch('/api/usersLogin', requestParams)
-      .then(data => data.json())
+      .then(data => {
+        if (data.ok) {
+          return data.json();
+        }
+        this.setState({
+          userName: '',
+          password: '',
+          hasUserUnsuccessfullyLoggedIn: true
+        });
+        throw new Error('Log in unsuccessful.');
+      })
       .then(userData => {
         this.props.logInUser(userData);
         this.props.history.push('/');
@@ -46,14 +57,29 @@ export default class LogInPage extends React.Component {
   render() {
     return (
       <div className="log-in-container">
+        <div className="log-in-header">
+          <span className="back-button" onClick={() => this.props.history.goBack()}>
+            <i className="fa fa-angle-double-left"></i>
+          </span>
+        </div>
         <ShadowRealmerIcon />
         <div className="welcome-banner">
           Welcome to Shadow Realmer
         </div>
         <form onSubmit={this.handleSubmit}>
           <div className="log-in-form-children-container">
-            <input type="text" className="long-input input" placeholder="Username" onChange={this.handleUserNameChange} value={this.state.userName} />
-            <input type="password" className="long-input input" placeholder="Password" onChange={this.handlePasswordChange} value={this.state.password} />
+            <input
+              type="text"
+              className={`long-input input ${this.state.hasUserUnsuccessfullyLoggedIn && 'log-in-error-border'}`}
+              placeholder="Username"
+              onChange={this.handleUserNameChange}
+              value={this.state.userName} />
+            <input
+              type="password"
+              className={`long-input input ${this.state.hasUserUnsuccessfullyLoggedIn && 'log-in-error-border'}`}
+              placeholder="Password"
+              onChange={this.handlePasswordChange}
+              value={this.state.password} />
             <div className="log-in-form-button-container">
               <button type="submit" className="short-input input confirm">Log In</button>
               <button className="short-input input green" onClick={() => this.props.history.push('/sign-up')}>Sign Up</button>
