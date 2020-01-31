a6d9b4dff541b24ee217d902450f212e80225ade
spenceruns <spenceruns@gmail.com>
2020-01-31 16:54:58 +0000

    added found stores and the map center to context so that it does not change whenever you go to a new page.

--- client/components/store-finder-page.jsx
+++ client/components/store-finder-page.jsx
@@ -1,39 +1,20 @@
 import React from 'react';
 import NavBar from './navbar';
 import Map from './map';
+import SRContext from './context';
 
-export default class StoreFinder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stores: [],
-      center: { lat: 33.634844, lng: -117.740513 }
-    };
-    this.findStores = this.findStores.bind(this);
-  }
-
-  findStores(zipcode) {
-    const getCenter = fetch(`/api/zipcode/?zipcode=${zipcode}`).then(res => res.json());
-    const getResults = fetch(`/api/search/?zipcode=${zipcode}`).then(res => res.json());
-    Promise
-      .all([getCenter, getResults])
-      .then(result => {
-        this.setState({
-          center: result[0].results[0].geometry.location,
-          stores: result[1].results
-        });
-      })
-      .catch(err => console.error(err));
-  }
-
-  render() {
-    return (
-      <>
-        <NavBar {...this.props} runSearch={this.findStores} placeholder='Enter Zip Code'/>
-        <div className="map-container">
-          <Map zoom={12} center={this.state.center} stores={this.state.stores} />
-        </div>
-      </>
-    );
-  }
+export default function StoreFinder(props) {
+  return (
+    <SRContext.Consumer>{context => {
+      return (
+        <>
+          <NavBar {...props} runSearch={context.findStores} />
+          <div className="map-container">
+            <Map zoom={12} center={context.currentView.center} stores={context.currentView.stores} />
+          </div>
+        </>
+      );
+    }}
+    </SRContext.Consumer>
+  );
 }
